9eab1e33e75a036194d6228b8c10be08305da576
vaibhavdp <31156849+vaibhavdp@users.noreply.github.com>
2017-09-07 01:34:06 +0530

    First version
    Code to recusrsively list the files in current folder and each sub-folder

---
+++ FileLister/FileLister.WinForms/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace FileLister.WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.lblCommand = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtSelectedFolderPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(619, 47);
+            this.btnSelectFolder.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(147, 33);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select Folder...";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // lblCommand
+            // 
+            this.lblCommand.AutoSize = true;
+            this.lblCommand.Location = new System.Drawing.Point(7, 18);
+            this.lblCommand.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblCommand.Name = "lblCommand";
+            this.lblCommand.Size = new System.Drawing.Size(244, 26);
+            this.lblCommand.TabIndex = 1;
+            this.lblCommand.Text = "Enter the Root Folder path:";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // txtSelectedFolderPath
+            // 
+            this.txtSelectedFolderPath.Location = new System.Drawing.Point(12, 47);
+            this.txtSelectedFolderPath.Name = "txtSelectedFolderPath";
+            this.txtSelectedFolderPath.Size = new System.Drawing.Size(598, 33);
+            this.txtSelectedFolderPath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Output: c:\\temp\\list.csv";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 259);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSelectedFolderPath);
+            this.Controls.Add(this.lblCommand);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FileLister";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Label lblCommand;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtSelectedFolderPath;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FileLister/FileLister.WinForms/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileLister.WinForms
+{
+    public partial class Form1 : Form
+    {
+        #region public contructor
+        public Form1()
+        {
+            InitializeComponent();
+            m_OutputFilePath = String.Format("c:\\temp\\List_{0}.txt", DateTime.Now.Ticks.ToString());
+        }
+        #endregion
+
+        private string m_SelectedFolder { get; set; }
+        private string m_OutputFilePath { get; set; }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult dResult = folderBrowserDialog1.ShowDialog();
+
+            if (dResult != DialogResult.OK)
+            {
+                MessageBox.Show("Please select the parent folder !");
+            }
+
+            m_SelectedFolder = folderBrowserDialog1.SelectedPath;
+
+            txtSelectedFolderPath.Text = m_SelectedFolder;
+
+            // Start BackgroundWorker
+            backgroundWorker1.RunWorkerAsync(m_SelectedFolder);
+
+        }
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string pathToProcess = e.Argument as string;
+            ListFiles(pathToProcess);
+        }
+
+        private void ListFiles(string path)
+        {
+            if (String.IsNullOrWhiteSpace(path))
+            {
+                return;
+            }
+
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+            #region Check and Iterate File names
+
+            FileInfo[] files = dirInfo.GetFiles();
+            if (files != null && files.Length > 0)
+            {
+                int index = 1;
+                List<string> mp3FileNames = (from f in files
+                                             where f.Extension.ToLower().Contains("mp3")
+                                             select String.Format("<tr><td>{0}</td><td>{1}</td></tr>", index++, f.Name.Replace(",", " "))).ToList<string>();
+
+                if (mp3FileNames != null && mp3FileNames.Count > 0)
+                {
+                    mp3FileNames.Insert(0, String.Format("<tr><td>&nbsp;</td><td>&nbsp;</td></tr><tr><td>FolderName:</td><td>{1}</td></tr>", 
+                        System.Environment.NewLine,  dirInfo.FullName.Replace(",", " ")));
+
+                    File.AppendAllLines(m_OutputFilePath, mp3FileNames);
+                }
+            }
+            #endregion
+
+            #region Check and Recurse on Folders
+
+            DirectoryInfo[] subFolders = dirInfo.GetDirectories();
+
+            if (subFolders != null && subFolders.Length > 0)
+            {
+                foreach (DirectoryInfo subFolder in subFolders)
+                {
+                    ListFiles(subFolder.FullName);
+                }
+            }
+
+            #endregion
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Process completed...");
+        }
+    }
+}
